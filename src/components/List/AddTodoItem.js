e35f44fb5daa17dbc21c4f5aba4354371633efc6
TFVO11 <cosmoyj@naver.com>
2022-09-26 20:21:07 +0900

    adding_errorStateInAddToDoItem

--- src/components/List/AddTodoItem.js
+++ src/components/List/AddTodoItem.js
@@ -4,16 +4,30 @@ import { useState } from "react";
 import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 
 import classes from "./AddTodoItem.module.css";
 
 const AddTodoItem = (props) => {
     const [addingTodoItem, setAddingTodoItem] = useState("");
 
+    const [error, setError] = useState();
+
     const handleSubmit = (event) => {
         event.preventDefault();
         props.onAddTodoItem(addingTodoItem);
         setAddingTodoItem("");
+        if (addingTodoItem.trim().length === 0){
+            setError({
+                title: 'invalid input',
+                messege: 'Please enter a valid name and age (non-empyt values)'
+            });
+            return console.log("error");
+        }
+    };
+
+    const handleError = () => {
+        setError(null);
     };
 
     const handleChangeInput = (event) => {
@@ -22,6 +36,12 @@ const AddTodoItem = (props) => {
 
     return (
         <Wrapper>
+            {/* {error && (
+                <ErrorModal
+                title={error.title}
+                messege={error.message}
+                />
+            )} */}
             <Card className={classes.input}>
                 <form onSubmit={handleSubmit}>
                     <label>Enter List</label>
